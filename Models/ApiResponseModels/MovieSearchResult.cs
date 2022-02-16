3fd03fb855c3444d6cd887b08e94f8bf438bb3f4
Zsani <horvathzsanett.95@gmail.com>
2022-02-16 16:38:54 +0100

    Add project files.

---
+++ Models/ApiResponseModels/MovieSearchResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApplication.Models.ApiResponseModels
+{
+    //Model of the IMDb api's movie search result
+    public class MovieSearchResult
+    {
+        public List<d> d { get; set; }
+
+        public string q { get; set; }
+
+        public int v { get; set; }
+    }
+
+    public class d
+    {
+        public string id { get; set; }
+
+        public string l { get; set; }
+    }
+}
---
+++ Services/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using MovieApplication.Models.ApiResponseModels;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MovieApplication.Services
+{
+    public class MovieService
+    {
+        public async Task<MovieSearchResult> GetMovieAsync()
+        {
+            var client = new RestClient("https://imdb8.p.rapidapi.com/title/auto-complete?q=venom");
+            var request = new RestRequest();
+            request.AddHeader("x-rapidapi-host", "imdb8.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "a5ae78cc18mshf93e0d1908b538bp1584cbjsnbcaba329eaf9");
+            RestResponse response =  await client.ExecuteAsync(request);
+            MovieSearchResult movie =
+                JsonSerializer.Deserialize<MovieSearchResult>(response.Content);
+            return movie;
+        }
+    }
+}
